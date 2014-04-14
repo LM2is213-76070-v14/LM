c1764f24c125a99d310518ead891695d7876f948
Lisa Broman <Lisa@192.168.1.7>
2014-04-14 11:35:15 +0200

    Ny output

---
+++ LM/src/lm/Main.java
@@ -0,0 +1,45 @@
+package lm;
+
+public class Main {
+
+public static void main(String[] args) {
+
+//ADDED
+System.out.println("Lantmäteriet");
+
+//ADDED from JKiiskinen
+System.out.println("Nu har jag gjort en ändrig");
+
+//added onsdag
+System.out.println("...och idag är det onsdag igen för oss alla.");
+
+//added uu
+System.out.println("Heja UU!");
+
+//ADDED
+System.out.println("Min andra ändring");
+
+//ADDED by Lisa
+System.out.println("Det går bra nu!");
+
+//ADDED
+System.out.println("Snart lunch kanske?");
+
+//ADDED new
+System.out.println("This is a new printout.");
+
+//Added a new output 14042014 Jonathan
+System.out.println("This is a new printout.");
+
+//Added a new output Patrik
+System.out.println("This is a another new printout.");
+
+//Added a new output Patrik again
+System.out.println("This is a another new printout again.");
+
+//Added a new output Lisa
+System.out.println("Snart klart nu");
+
+}
+
+}
