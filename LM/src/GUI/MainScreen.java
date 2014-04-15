e895c89db8d92e4e62ad2d00eb8e655f47da8641
JKiiskinen <jonkii01@student.hgo.se>
2014-04-15 12:14:30 +0200

    cleaned up code and redirected start of program to Main in logic

--- LM/src/GUI/MainScreen.java
+++ LM/src/GUI/MainScreen.java
@@ -7,118 +7,62 @@ import java.awt.event.*;
 public class MainScreen extends JFrame {
     
     public MainScreen() {
-        
+        createMainScreen();
+    }
+    
+    public void createMainScreen() {
+    	
     	JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-		
-		JMenu menuFile = new JMenu("File");
-		menuBar.add(menuFile);
-		
-		JMenuItem itemNew = new JMenuItem("New");
-		menuFile.add(itemNew);
-		
-		JMenuItem itemOpen = new JMenuItem("Open");
-		menuFile.add(itemOpen);
-		
-		JMenuItem itemSave = new JMenuItem("Save");
-		menuFile.add(itemSave);
-		
-		JMenuItem itemPrint = new JMenuItem("Print");
-		menuFile.add(itemPrint);
-		
-		JMenuItem itemExit = new JMenuItem("Exit");
-		menuFile.add(itemExit);
-		
-		JMenu menuEdit = new JMenu("Edit");
-		menuBar.add(menuEdit);
-		
-		JMenuItem itemDelete = new JMenuItem("Delete");
-		menuEdit.add(itemDelete);
-		
-		JMenuItem itemArchive = new JMenuItem("Archive");
-		menuEdit.add(itemArchive);
-		
-		JMenu menuHelp = new JMenu("Help");
-		menuBar.add(menuHelp);
-		
-		JMenuItem itemHelp = new JMenuItem("Help");
-		menuHelp.add(itemHelp);
-		
-		JMenuItem itemAbout = new JMenuItem("About");
-		menuHelp.add(itemAbout);
     	
          //This will create the title you see in the upper left of the window    
         setTitle("Tabbed Pane");  
         setSize(600,400); //set size so the user can "see" it
         //Here we are creating the object
-     
                
-       JTabbedPane jtp = new JTabbedPane();
+       JTabbedPane tabPanel = new JTabbedPane();
              
        //Moves tabs to the right side
-       jtp.setTabPlacement(MAXIMIZED_HORIZ);
-       
+       tabPanel.setTabPlacement(MAXIMIZED_HORIZ);
        
         //This creates the template on the windowed application that we will be using
-       getContentPane().add(jtp);
+       getContentPane().add(tabPanel);
 
-       JPanel jp1 = new JPanel();//This will create the first tab
+       JPanel updateTab = new JPanel();//This will create the first tab
 
-       JPanel jp2 = new JPanel();//This will create the second tab
+       JPanel generalTab = new JPanel();//This will create the second tab
        
-       JPanel jp3 = new JPanel();//This will create the third tab
+       JPanel disruptionsTab = new JPanel();//This will create the third tab
        
-       JPanel jp4 = new JPanel();//This will create the fourth tab
+       JPanel createMessageTab = new JPanel();//This will create the fourth tab
        
-       JPanel jp5 = new JPanel();//This will create the fifth tab
+       JPanel editMessageTab = new JPanel();//This will create the fifth tab
 
-       JPanel jp6 = new JPanel();//This will create the sixth tab
+       JPanel editSubTab = new JPanel();//This will create the sixth tab
        
-       JPanel jp7 = new JPanel();//This will create the seventh tab
+       JPanel mySubTab = new JPanel();//This will create the seventh tab
        
-       JPanel jp8 = new JPanel();//This will create the eight tab
+       JPanel myAccountTab = new JPanel();//This will create the eight tab
        
          //This creates a non-editable label, sets what the label will read
         //and adds the label to the first tab
        JLabel label1 = new JLabel();
-       jp1.add(label1);
-       ShowMessages showMessages = new ShowMessages(jp1);
-       jp1.add(showMessages.getPanel());
+       updateTab.add(label1);
+       ShowMessages showMessages = new ShowMessages(updateTab, menuBar);
+       updateTab.add(showMessages.getPanel());
 
        //This adds the first and second tab to our tabbed pane object and names it
-       jtp.addTab("Updates", jp1);
-       jtp.addTab("General info", jp2);
-       jtp.addTab("Disruptions", jp3);
-       jtp.addTab("Create message", jp4);
+       tabPanel.addTab("Updates", updateTab);
+       tabPanel.addTab("General info", generalTab);
+       tabPanel.addTab("Disruptions", disruptionsTab);
+       tabPanel.addTab("Create message", createMessageTab);
 
        
-       jtp.addTab("Edit message", jp5);
-       jtp.addTab("Edit subscription", jp6);
-       jtp.addTab("My subscriptions", jp7);
-       jtp.addTab("My account", jp8);
-       
-        //This creates a new button called "Press" and adds it to the second tab
-       JButton test = new JButton("Press");
-       jp2.add(test);
+       tabPanel.addTab("Edit message", editMessageTab);
+       tabPanel.addTab("Edit subscription", editSubTab);
+       tabPanel.addTab("My subscriptions", mySubTab);
+       tabPanel.addTab("My account", myAccountTab);
 
-        //This is an Action Listener which reacts to clicking on 
-        //the test button called "Press"
-        ButtonHandler phandler = new ButtonHandler();
-        test.addActionListener(phandler);
         setVisible(true); //otherwise you won't "see" it 
     }
-    
-    //This is the internal class that defines what the above Action Listener
-    //will do when the test button is pressed.
-    class ButtonHandler implements ActionListener{
-           public void actionPerformed(ActionEvent e){
-                   JOptionPane.showMessageDialog(null, "I've been pressed", "What happened?", JOptionPane. INFORMATION_MESSAGE);
-           }
-    }
-
-    //example usage
-     public static void main (String []args){
-    	 MainScreen tab = new MainScreen();
-    }
-
 }
--- LM/src/GUI/ShowMessages.java
+++ LM/src/GUI/ShowMessages.java
@@ -3,9 +3,13 @@ package GUI;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+
 import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
 import javax.swing.JList;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
@@ -23,9 +27,10 @@ public class ShowMessages {
 	/**
 	 * This is the constructor
 	 */
-	public ShowMessages(JPanel mainPanel) {
+	public ShowMessages(JPanel mainPanel, JMenuBar menuBar) {
 		mMessageList = new MessageList();
 		createPanel(mainPanel);
+		createMenuBar(menuBar);
 	}
 
 	/**
@@ -74,6 +79,44 @@ public class ShowMessages {
 		}
 
 		contentPane.add(pane);
+	}
+	
+	public void createMenuBar(JMenuBar menuBar) {
+		JMenu menuFile = new JMenu("File");
+		menuBar.add(menuFile);
+		
+		JMenuItem itemNew = new JMenuItem("New");
+		menuFile.add(itemNew);
+		
+		JMenuItem itemOpen = new JMenuItem("Open");
+		menuFile.add(itemOpen);
+		
+		JMenuItem itemSave = new JMenuItem("Save");
+		menuFile.add(itemSave);
+		
+		JMenuItem itemPrint = new JMenuItem("Print");
+		menuFile.add(itemPrint);
+		
+		JMenuItem itemExit = new JMenuItem("Exit");
+		menuFile.add(itemExit);
+		
+		JMenu menuEdit = new JMenu("Edit");
+		menuBar.add(menuEdit);
+		
+		JMenuItem itemDelete = new JMenuItem("Delete");
+		menuEdit.add(itemDelete);
+		
+		JMenuItem itemArchive = new JMenuItem("Archive");
+		menuEdit.add(itemArchive);
+		
+		JMenu menuHelp = new JMenu("Help");
+		menuBar.add(menuHelp);
+		
+		JMenuItem itemHelp = new JMenuItem("Help");
+		menuHelp.add(itemHelp);
+		
+		JMenuItem itemAbout = new JMenuItem("About");
+		menuHelp.add(itemAbout);
 	}
 
 	/**
